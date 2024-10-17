77c65d3f3f70dad42b57954ff652ab89ba64c2bc
olegv101 <147751361+olegv101@users.noreply.github.com>
2024-10-16 23:47:18 -0700

    added better sidebar

--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -1,3 +1,4 @@
+import { ul } from 'framer-motion/client';
 import React from 'react';
 
 const sidebarItems = [
@@ -10,20 +11,20 @@ const sidebarItems = [
 
 const Sidebar: React.FC = () => {
   return (
-    <div className="w-30 bg-[#141313] text-white h-screen fixed left-0 top-20">
+    <div className="w-30 bg-[#141313] text-white h-screen fixed left-0 top-20 -mt-3 border-r-[1px] border-[#555555]">
       <nav className="h-4">
         <ul>
           {sidebarItems.map((item, index) => (
             <li key={index} className="relative group">
               <a
                 href="#"
-                className="block px-4 py-2 h-20 hover:bg-gray-800 transition-colors duration-200 flex flex-col justify-end"
+                className="translate-x-[1px] px-4 py-2 h-20 transition-all duration-300 flex flex-col justify-end relative overflow-hidden group hover:bg-gradient-to-r hover:from-[#E073FF]/10 hover:to-transparent before:absolute before:inset-0 before:bg-gradient-to-r before:from-[#E073FF]/10 before:to-transparent before:translate-x-[-100%] hover:before:translate-x-0 before:transition-transform before:duration-300 border-b border-r border-[#555555]"
               >
-                <div className="mb-2">
+                <div className="mb-2 flex flex-col justify-end ">
                   {item.subtitle && (
-                    <span className="text-xs text-purple-400 block">{item.subtitle}</span>
+                    <span className="text-xs text-purple-400 block ">{item.subtitle}</span>
                   )}
-                  <div className="text-sm break-words">{item.title}</div>
+                  <div className="text-sm break-words group-hover:translate-x-2 transition-transform duration-300 ease-in-out">{item.title}</div>
                 </div>
               </a>
             </li>
