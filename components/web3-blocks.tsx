df635e493ccfe44e560df4ceae72efcf25e893a8
olegv101 <147751361+olegv101@users.noreply.github.com>
2024-10-17 03:55:37 -0700

    smaller nodes

--- components/web3-blocks.tsx
+++ components/web3-blocks.tsx
@@ -183,14 +183,14 @@ export default function Web3BlocksComponent() {
                         initial={{ opacity: 0, y: 20 }}
                         animate={{ opacity: 1, y: 0 }}
                         exit={{ opacity: 0, y: -20 }}
-                        className={`${block.color} text-white p-4 rounded-lg shadow-md cursor-move select-none
+                        className={`${block.color} text-white p-6 rounded-lg shadow-md cursor-move select-none
                                     flex items-center justify-between border-2 ${block.borderColor} hover:border-[#FB118E] transition-colors w-full max-w-[400px] relative`}
                         onMouseEnter={() => setHoveredBlock(block.uniqueId)}
                         onMouseLeave={() => setHoveredBlock(null)}
                       >
                         <span>{block.content}</span>
-                        <block.icon className="w-5 h-5" />
-                        <div className="absolute right-[-10px] top-1/2 transform -translate-y-1/2 w-4 h-8 bg-white rounded-full"></div>
+                        <block.icon className="w-4 h-4" />
+                        <div className="absolute right-[-7px] top-1/2 transform -translate-y-1/2 w-3 h-6 bg-white rounded-full"></div>
                         {hoveredBlock === block.uniqueId && (
                           <div className="absolute inset-0 bg-black bg-opacity-50 flex items-center justify-center rounded-lg">
                             <span className="text-white">Hover effect</span>
