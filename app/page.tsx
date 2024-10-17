80cf00d6fd6e32bc21a32c9047ca3b0958ef8d05
olegv101 <147751361+olegv101@users.noreply.github.com>
2024-10-17 00:47:13 -0700

    added swap component

--- app/page.tsx
+++ app/page.tsx
@@ -7,10 +7,13 @@ export default function Home() {
   return (
     <div className="">
       <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        {/* <Web3BlocksComponent /> */}
         <Navbar />
+        <div className="pl-32 w-full -mt-8">
+          <Web3BlocksComponent />
+        </div>
+
         <Sidebar />
-        </main>
+      </main>
       <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
         <p>Made by Sanjay and Oleg</p>
       </footer>
