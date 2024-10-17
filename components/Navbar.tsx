3ef8ddc14576169c5818f031b87309c6a4396c5a
olegv101 <147751361+olegv101@users.noreply.github.com>
2024-10-17 03:19:30 -0700

    added cheeky animation s

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -9,10 +9,11 @@ export default function Navbar() {
 
     return (
         <nav className="fixed w-full bg-[#141313] text-white border-b border-[#555555]">
-            <div className="container mx-auto flex items-center justify-between">
+            
+            <div className="px-10 mx-auto flex items-center justify-between">
                 <div className="flex flex-row space-x-4 ">
-                    <div className="flex items-center space-x-4 flex-grow">
-                        <div className="flex items-center space-x-2">
+                    <div className="flex items-center space-x-4 flex-grow mr-12">
+                        <div className="flex items-center space-x-2 mr-1">
                             <div className="w-6 h-6 bg-purple-500 rounded-full"></div>
                             <span className="font-bold">WTV PROTOCOL</span>
                         </div>
@@ -41,8 +42,11 @@ export default function Navbar() {
 
 function NavItem({ icon, text }: { icon: React.ReactNode; text: string }) {
     return (
-        <div className="h-16 px-10 justify-center items-center inline-flex border-white/30 border-x-[1px] cursor-pointer">
-            <div className="text-white text-base font-normal font-['Satoshi Variable']">{text}</div>
+        <div className="group h-16 px-10 justify-center items-center inline-flex border-white/30 border-x-[1px] cursor-pointer overflow-hidden relative">
+            <div className="text-white text-sm font-normal font-['Satoshi Variable'] z-10 group-hover:-translate-y-1 transition-transform duration-300 ease-in-out">{text}</div>
+            <div className="absolute inset-0 flex items-center justify-center">
+                <div className="w-64 h-64 translate-y-full group-hover:translate-y-1/2 blur-md rounded-full bg-gradient-to-t from-[#E073FF]/50 to-[#E073FF]/10 opacity-0 group-hover:opacity-100 transition-all duration-300 transform"/>
+            </div>
         </div>
     );
 }
