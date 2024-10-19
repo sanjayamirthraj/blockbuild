517fb3403fc7704056d08095c58f014752064dfc
Sanjay Amirthraj <sanjay.amirthraj@gmail.com>
2024-10-19 05:13:58 -0700

    added rootstock

--- lib/wagmi.ts
+++ lib/wagmi.ts
@@ -1,10 +1,11 @@
 import { http, createConfig } from 'wagmi'
-import { mainnet, sepolia } from 'wagmi/chains'
+import { mainnet, sepolia, rootstockTestnet } from 'wagmi/chains'
 
 export const config = createConfig({
-    chains: [mainnet, sepolia],
+    chains: [mainnet, sepolia, rootstockTestnet],
     transports: {
         [mainnet.id]: http(),
         [sepolia.id]: http(),
+        [rootstockTestnet.id]: http(),
     },
 })
