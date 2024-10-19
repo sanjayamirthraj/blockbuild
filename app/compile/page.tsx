a468a2f7549bd19c838382e0e6d4a2a3492da0a2
olegv101 <147751361+olegv101@users.noreply.github.com>
2024-10-19 04:15:46 -0700

    checkpoint for boof flow graph on compile page

--- app/compile/page.tsx
+++ app/compile/page.tsx
@@ -5,35 +5,32 @@ import FlowGraph from '@/app/compile/FlowGraph';
 import ContractDeployment from './ContractDeployement';
 import ContractCode from './ContractCode';
 import { Button } from '@/components/ui/button';
+import Sidebar from "../../components/Sidebar";
+import Navbar from "../../components/Navbar";
 
 const CompilePage: React.FC = () => {
-  return (
-    <div className="flex flex-col min-h-screen bg-gray-900 text-white">
-      <header className="p-4">
-        <Button variant="outline" onClick={() => { /* Handle go back */ }}>
-          ← Go Back
-        </Button>
-      </header>
-      
-      <main className="flex-grow flex">
-        <section className="w-1/2 p-4">
-          <h2 className="text-2xl mb-4">Flow Graph</h2>
-          <FlowGraph />
-        </section>
-        
-        <section className="w-1/2 p-4">
-          <ContractDeployment />
-          <ContractCode />
-        </section>
-      </main>
-      
-      <footer className="p-4 flex justify-end">
-        <Button onClick={() => { /* Handle finish */ }}>
-          Finish
-        </Button>
-      </footer>
-    </div>
-  );
+    return (
+        <div className="flex flex-col min-h-screen text-white">
+            <Navbar />
+            <Sidebar />
+
+            <div className="flex-grow flex w-full px-32 py-16">
+
+                <main className="flex-grow flex">
+                    <section className="w-1/2 p-4">
+                        <h2 className="text-2xl mb-4">Flow Graph</h2>
+                        <FlowGraph />
+                    </section>
+
+                    <section className="w-1/2 p-4">
+                        <ContractDeployment />
+                        <ContractCode />
+                    </section>
+                </main>
+            </div>
+
+        </div>
+    );
 };
 
 export default CompilePage;
