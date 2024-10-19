f699b6a9f683c66639e1666dc5e6d3e4377153ba
Sanjay Amirthraj <sanjay.amirthraj@gmail.com>
2024-10-19 05:09:48 -0700

    Wagmi hooks work. Need to do e2e testing

---
+++ app/providers.tsx
@@ -0,0 +1,17 @@
+'use client'
+import { WagmiProvider } from "wagmi";
+import { config } from '../lib/wagmi'
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+
+const queryClient = new QueryClient()
+
+export function Providers({ children,
+}: Readonly<{
+  children: React.ReactNode;
+}> ) {
+    return <WagmiProvider config={config}>
+        <QueryClientProvider client={queryClient}>
+            {children}
+        </QueryClientProvider>
+    </WagmiProvider>
+}
