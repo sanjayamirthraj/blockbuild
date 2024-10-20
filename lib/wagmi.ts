0e42ff147b66959507a716c876772466d196bc93
Sanjay Amirthraj <sanjay.amirthraj@gmail.com>
2024-10-19 23:42:09 -0700

    xrpl stuff

--- lib/wagmi.ts
+++ lib/wagmi.ts
@@ -3,25 +3,25 @@ import { rootstockTestnet } from 'wagmi/chains'
 import { metaMask } from "wagmi/connectors";
 import { type Chain } from 'viem'
 
-// export const xrplEvmSidechainDevnet = {
-//     id: 1440002,
-//     name: 'XRPL EVM Sidechain Devnet',
-//     nativeCurrency: { name: 'XRP', symbol: 'XRP', decimals: 6 },
-//     rpcUrls: {
-//         default: { http: ['https://rpc-evm-sidechain.xrpl.org'] },
-//     },
-//     blockExplorers: {
-//         default: { name: 'XRPL EVM Explorer', url: 'https://explorer.xrplevm.org' },
-//     },
-//     contracts: {
-//     },
-// } as const satisfies Chain
+export const xrplEvmSidechainDevnet = {
+    id: 1440002,
+    name: 'XRPL EVM Sidechain Devnet',
+    nativeCurrency: { name: 'XRP', symbol: 'XRP', decimals: 6 },
+    rpcUrls: {
+        default: { http: ['https://rpc-evm-sidechain.xrpl.org'] },
+    },
+    blockExplorers: {
+        default: { name: 'XRPL EVM Explorer', url: 'https://explorer.xrplevm.org' },
+    },
+    contracts: {
+    },
+} as const satisfies Chain
 
 export const config = createConfig({
-    chains: [rootstockTestnet],
+    chains: [xrplEvmSidechainDevnet],
     connectors: [metaMask()],
     transports: {
-        [rootstockTestnet.id]: http(),
+        [xrplEvmSidechainDevnet.id]: http(),
     },
 })
 
